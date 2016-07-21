b29f27cec91a9d0fc71248ebb47cd0fe70490d1b
Miguel Mendoza <miguel.mendoza@vizzuality.com>
2016-07-22 01:31:43 +0200

    adds network projects controller

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,15 @@ Rails.application.routes.draw do
   get '/',          to: 'home#index',      as: 'home'
   get '/map',       to: 'map#index',       as: 'map'
   get '/countries', to: 'countries#index', as: 'countries'
-  get '/cancers',   to: 'cancers#index',   as: 'cancers'
+  get '/cancer-types',   to: 'cancer_types#index',   as: 'cancers'
   get '/about',     to: 'about#index',     as: 'about'
+
+  # User profile projects
+  get 'network/:user_id/projects', to: 'network_projects#index', as: 'user_projects'
+  get 'network/:user_id/projects/:project_id', to: 'network_projects#show', as: 'user_project'
+  get 'network/projects/new', to: 'network_projects#new', as: 'new_user_project'
+  get 'network/projects/create', to: 'network_projects#create', as: 'create_user_project'
+  get 'network/projects/edit/:project_id', to: 'network_projects#edit', as: 'edit_user_project'
+  get 'network/projects/update/:project_id', to: 'network_projects#update', as: 'update_user_project'
+  get 'network/projects/delete/:project_id', to: 'network_projects#delete', as: 'delete_user_project'
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160720162924) do
+ActiveRecord::Schema.define(version: 20160721141137) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -107,6 +107,19 @@ ActiveRecord::Schema.define(version: 20160720162924) do
     t.text     "website"
     t.datetime "created_at",     null: false
     t.datetime "updated_at",     null: false
+  end
+
+  create_table "members", force: :cascade do |t|
+    t.integer  "project_id"
+    t.integer  "organization_id"
+    t.integer  "investigator_id"
+    t.integer  "membership_type"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+    t.index ["investigator_id"], name: "index_members_on_investigator_id", using: :btree
+    t.index ["membership_type"], name: "index_members_on_membership_type", using: :btree
+    t.index ["organization_id"], name: "index_members_on_organization_id", using: :btree
+    t.index ["project_id"], name: "index_members_on_project_id", using: :btree
   end
 
   create_table "organization_types", force: :cascade do |t|
