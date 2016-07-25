c16a548fb0a529a0bc68f005d8b65e6ddb393915
Miguel Mendoza <miguel.mendoza@vizzuality.com>
2016-07-25 22:53:42 +0200

    capify

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,6 +1,6 @@
 server ENV['STAGING_IP'], user: ENV['SSH_USER'], roles: %w{web app db}, primary: true
 set :ssh_options, {
-  forward_agent: true
-  uth_methods: %w(publickey password),
+  forward_agent: true,
+  auth_methods: %w(publickey password),
   password: fetch(:password)
 }
