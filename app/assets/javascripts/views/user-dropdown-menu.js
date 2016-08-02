86a26d2fa73c6cce889b597c24c9dc77edebbfc2
bitTal <albafjez@gmail.com>
2016-08-02 10:38:54 +0200

    User dropdown hides when clicking outside

--- app/assets/javascripts/views/user-dropdown-menu.js
+++ app/assets/javascripts/views/user-dropdown-menu.js
@@ -8,24 +8,57 @@
 
     el: '.user-avatar',
 
-    // Events not working, that's why listeners are set up
+    model: new (Backbone.Model.extend({
+      defaults: {
+        hidden: true
+      }
+    })),
+
     events: {
-      'click .user-avatar': 'toggleDropdown'
+      'click': 'setHidden'
     },
 
     initialize: function() {
+      this.cache();
       this.listeners();
     },
 
-    listeners() {
-      var avatar = document.getElementsByClassName('user-avatar')[0];
-      avatar.addEventListener('click', this.toggleDropdown);
+    cache: function() {
+      this.$dropdownMenu = this.$el.find('.js-user-dropdown-menu');
+      this.$document = $(document);
     },
 
-    toggleDropdown: function(e) {
-      e.preventDefault();
-      var dropdownMenu = document.querySelector('.js-user-dropdown-menu');
-      dropdownMenu.classList.toggle('-open');
+    listeners: function(){
+      this.model.on('change:hidden', this.changeHidden.bind(this));
+    },
+
+    // UI Events
+    setHidden: function(e) {
+      var hidden = this.model.get('hidden');
+      this.model.set('hidden', !hidden);
+    },
+
+    // Changes
+    changeHidden: function() {
+      this.$dropdownMenu.toggleClass('-open', !this.model.get('hidden'));
+      !this.model.get('hidden') ? this.setBindings() : this.unsetBindings() ;
+    },
+
+     /**
+     * BINDINGS
+     * - setBindings
+     * - unsetBindings
+     */
+    setBindings: function() {
+      this.$document.on('click.user-dropdown', function(e) {
+        if(!this.el.contains(e.target) && this.el !== e.target) {
+          this.model.set('hidden', true);
+        }
+      }.bind(this));
+    },
+
+    unsetBindings: function() {
+      this.$document.off('click.user-dropdown');
     }
 
   });
