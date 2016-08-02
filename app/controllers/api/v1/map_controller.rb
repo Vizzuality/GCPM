c9cc77a75e31d52e7078d6037ccb750ad8016400
Miguel Mendoza <miguel.mendoza@vizzuality.com>
2016-08-02 13:56:06 +0200

    changes json keys

--- app/controllers/api/v1/map_controller.rb
+++ app/controllers/api/v1/map_controller.rb
@@ -4,17 +4,22 @@ module Api
       def index
         case params[:group]
         when 'regions'
-          query = SqlQuery.new(:map_regions)
+          query = SqlQuery.new(:map_regions, params: map_params)
         when 'countries'
-          query = SqlQuery.new(:map_countries)
+          query = SqlQuery.new(:map_countries, params: map_params)
         when 'projects'
-          query = SqlQuery.new(:map_projects)
+          query = SqlQuery.new(:map_projects, params: map_params)
         else
-          query = SqlQuery.new(:map_regions)
+          query = SqlQuery.new(:map_regions, params: map_params)
         end
         json_list = query.execute
         render json: json_list
       end
+
+      def map_params
+        params.permit(:start_date, :end_date, countries:[], cancer_types:[], organization_types:[], organizations:[])
+      end
     end
   end
 end
+
