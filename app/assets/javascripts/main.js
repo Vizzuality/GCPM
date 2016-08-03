0914d1b31e643ca0bf5dbe3ddbed94ec494f4982
bitTal <albafjez@gmail.com>
2016-08-03 10:22:33 +0200

    Project detail info section

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -27,6 +27,7 @@
       this.listenTo(this.router, 'route:map', this.mapPage);
       this.listenTo(this.router, 'route:countries', this.countriesPage);
       this.listenTo(this.router, 'route:country', this.countryPage);
+      this.listenTo(this.router, 'route:project', this.projectDetail);
       this.listenTo(this.router, 'route:network', this.userPage);
 
       // Listening magic links
@@ -118,6 +119,20 @@
     },
 
     userPage: function() {
+      var params = this.router.getParams();
+
+      // Map view
+      var layersCollection = new App.Collection.Layers();
+      var mapView = new App.View.Map({
+        layers: layersCollection,
+      });
+
+      layersCollection.toggleLayers([
+        params.type || 'org-project-markers'
+      ]);
+    },
+
+    projectDetail: function() {
       var params = this.router.getParams();
 
       // Map view
