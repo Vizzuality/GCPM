5131e4d874cdcb52b6f0290ec892b78e6d329088
Adrián <aloha@adrianperez.info>
2016-08-08 16:46:26 +0200

    stop default backbone save and send POST to API

--- app/assets/javascripts/models/investigator.model.js
+++ app/assets/javascripts/models/investigator.model.js
@@ -10,8 +10,33 @@
 
     parse: function(data) {
       return data;
-    }
+    },
 
-  });
+    save: function (key, val, options) {
+     this.beforeSave(key, val, options);
+     return this;
+     return Backbone.Model.prototype.save.call(this, key, val, options);
+   },
+
+	beforeSave: function (key, val, options) {
+		$.ajax({
+			type: "POST",
+			url: this.url+'?token='+AUTH_TOKEN,
+			data: JSON.stringify({
+			  "investigator" : {
+					"name" 		: this.attributes.name,
+					"email" 	: this.attributes.email,
+					"website" 	: this.attributes.website
+				}
+			}),
+			contentType: "application/json",
+			dataType: 'json',
+			complete: function(data) {
+			   console.log(data);
+			}
+		});
+	}
+
+});
 
 })(this.App);
