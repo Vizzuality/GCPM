bcfab6db0e87fbdfe9e4c2bf2d03e4032ee83df5
David Inga <davidsingal@icloud.com>
2016-08-09 13:55:21 +0200

    refactoring dispatcher and router

--- app/assets/javascripts/helpers/utils.js
+++ app/assets/javascripts/helpers/utils.js
@@ -22,6 +22,32 @@
     return o;
   };
 
+  // Extending Handlebars
+  Handlebars.registerHelper('ifCond', function (v1, operator, v2, options) {
+    switch (operator) {
+      case '==':
+        return (v1 == v2) ? options.fn(this) : options.inverse(this);
+      case '===':
+        return (v1 === v2) ? options.fn(this) : options.inverse(this);
+      case '!==':
+        return (v1 !== v2) ? options.fn(this) : options.inverse(this);
+      case '<':
+        return (v1 < v2) ? options.fn(this) : options.inverse(this);
+      case '<=':
+        return (v1 <= v2) ? options.fn(this) : options.inverse(this);
+      case '>':
+        return (v1 > v2) ? options.fn(this) : options.inverse(this);
+      case '>=':
+        return (v1 >= v2) ? options.fn(this) : options.inverse(this);
+      case '&&':
+        return (v1 && v2) ? options.fn(this) : options.inverse(this);
+      case '||':
+        return (v1 || v2) ? options.fn(this) : options.inverse(this);
+      default:
+        return options.inverse(this);
+    }
+  });
+
   App.Helper = App.Helper || {};
 
   /**
@@ -57,33 +83,6 @@
       });
 
       return params;
-    },
-
-    handlebarsHelpers: function() {
-      Handlebars.registerHelper('ifCond', function (v1, operator, v2, options) {
-        switch (operator) {
-          case '==':
-            return (v1 == v2) ? options.fn(this) : options.inverse(this);
-          case '===':
-            return (v1 === v2) ? options.fn(this) : options.inverse(this);
-          case '!==':
-            return (v1 !== v2) ? options.fn(this) : options.inverse(this);
-          case '<':
-            return (v1 < v2) ? options.fn(this) : options.inverse(this);
-          case '<=':
-            return (v1 <= v2) ? options.fn(this) : options.inverse(this);
-          case '>':
-            return (v1 > v2) ? options.fn(this) : options.inverse(this);
-          case '>=':
-            return (v1 >= v2) ? options.fn(this) : options.inverse(this);
-          case '&&':
-            return (v1 && v2) ? options.fn(this) : options.inverse(this);
-          case '||':
-            return (v1 || v2) ? options.fn(this) : options.inverse(this);
-          default:
-            return options.inverse(this);
-        }
-      });
     },
 
     getTimelinePercentage: function(startDate, endDate) {
--- app/assets/javascripts/router.js
+++ app/assets/javascripts/router.js
@@ -18,40 +18,9 @@
       // We are going to save params in model
       this.params = new (Backbone.Model.extend());
       // Listening events
-      this.on('route', this.runController);
       this.params.on('change', _.bind(this.updateUrl, this));
       // Global event to update params from external actions
       App.Events.on('Router:update', _.bind(this.updateParams, this));
-    },
-
-    /**
-     * Facilitates mapping URLs to controller actions
-     * based on a user-defined configuration file.
-     * It is responsible for observing and acting upon URL changes.
-     * @param  {String} routeName
-     * @param  {Array} routeParams
-     */
-    runController: function(routeName, routeParams) {
-      var routeSplited = routeName.split('#');
-      var controllerName = routeSplited[0];
-      var actionName = routeSplited[1];
-      var params = this.getParams(routeParams[0]);
-      if (App.Controller[controllerName] &&
-        App.Controller.hasOwnProperty(controllerName)) {
-        var currentController = new App.Controller[controllerName]();
-        // Checking if action exists
-        if (currentController[actionName] &&
-          typeof currentController[actionName] === 'function') {
-          // Setting new params in model
-          this.updateParams(params);
-          // Executing controller#action and passing url params
-          currentController[actionName](this.params.attributes);
-        } else {
-          console.error('specified action doesn\'t exist');
-        }
-      } else {
-        console.error('specified controller doesn\'t exist');
-      }
     },
 
     /**
