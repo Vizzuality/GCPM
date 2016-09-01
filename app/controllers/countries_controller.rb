f0d5facdab488e42245a08045455285a579a02bd
Adrián <aloha@adrianperez.info>
2016-09-01 17:45:14 +0200

    retrieve data and paint it

--- app/controllers/countries_controller.rb
+++ app/controllers/countries_controller.rb
@@ -5,5 +5,12 @@ class CountriesController < ApplicationController
 
   def show
     @country  = Country.find_by(country_iso: params[:iso])
+    @projects  = Project.fetch_all(projects_params).order('created_at DESC').limit(params[:limit] ? params[:limit].to_i * @limit : @limit)
+    @projectsCount = Project.count
+    @limit = 15
+  end
+  private
+  def projects_params
+    params.permit(:sortby, :user, :start_date, :end_date, project_types:[], countries:[], cancer_types:[], organization_types:[], organizations:[], regions:[], investigators:[])
   end
 end
