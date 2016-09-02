f3300a704f65d244729de3c6c654fc42454fb997
Adrián <aloha@adrianperez.info>
2016-09-02 11:02:16 +0200

    get country projects on the controller

--- app/controllers/countries_controller.rb
+++ app/controllers/countries_controller.rb
@@ -1,16 +1,19 @@
 class CountriesController < ApplicationController
+  before_action :set_country, only: :show
+
   def index
     @title = t 'countries'
   end
 
   def show
-    @country  = Country.find_by(country_iso: params[:iso])
-    @projects  = Project.fetch_all(projects_params).order('created_at DESC').limit(params[:limit] ? params[:limit].to_i * @limit : @limit)
-    @projectsCount = Project.count
+    @projects      = Project.fetch_all(countries: @country.id)
+    @projectsCount = @projects.length
     @limit = 15
   end
+
   private
-  def projects_params
-    params.permit(:sortby, :user, :start_date, :end_date, project_types:[], countries:[], cancer_types:[], organization_types:[], organizations:[], regions:[], investigators:[])
-  end
+    
+    def set_country
+      @country = Country.find_by(country_iso: params[:iso])
+    end
 end
