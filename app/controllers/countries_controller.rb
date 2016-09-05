353f019f804f43619c67209d16c08d14f78313b8
Adrián <aloha@adrianperez.info>
2016-09-05 13:30:46 +0200

    apply limit

--- app/controllers/countries_controller.rb
+++ app/controllers/countries_controller.rb
@@ -8,9 +8,9 @@ class CountriesController < ApplicationController
   def show
     @limit = 15
     @projects      = Project.fetch_all(countries: @country.id).order('created_at DESC').limit(params[:limit] ? params[:limit].to_i * @limit : @limit)
-    @projectsCount = @projects.length
+    @projectsCount = Project.fetch_all(countries: @country.id).length
     @events = Event.fetch_all(countries: @country.id).order('created_at DESC').limit(params[:limit] ? params[:limit].to_i * @limit : @limit)
-    @eventsCount = @events.length
+    @eventsCount = Event.fetch_all(countries: @country.id).length
 
     # @organizations  = Organization.fetch_all(countries: @country.id).order('created_at DESC').limit(params[:limit] ? params[:limit].to_i * @limit : @limit)
     # @organizationsCount = @organizations.length
