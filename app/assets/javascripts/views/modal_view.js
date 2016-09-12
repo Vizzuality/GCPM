f36ec3b320f6d38685a8a5e8c2c041519869f82e
David Inga <davidsingal@icloud.com>
2016-09-12 13:10:51 +0200

    added modal view and presenter

---
+++ app/assets/javascripts/views/modal_view.js
@@ -0,0 +1,51 @@
+(function(App) {
+
+  'use strict';
+
+  App.View.Modal = Backbone.View.extend({
+
+    events: {
+      'click .js-modal-backdrop': 'close',
+      'click .js-btn-modal-close': 'close'
+    },
+
+    template: HandlebarsTemplates['modal'],
+
+    initialize: function(settings) {
+      this.options = _.extend({}, settings);
+      this.$el.addClass('c-modal');
+      this.render();
+    },
+
+    render: function() {
+      this.$el.html(this.template());
+      this.$content = this.$el.find('.modal-content');
+    },
+
+    open: function(content) {
+      if (!content) {
+        return;
+      }
+      this.$el.appendTo('body');
+      if (typeof content === 'string') {
+        this.$content.html(content);
+      } else {
+        this.$content.html(content.render().$el);
+      }
+      this.$el.addClass('-active');
+    },
+
+    close: function(e) {
+      if (e) {
+        e.preventDefault();
+        e.stopPropagation();
+      }
+      this.$el.removeClass('-active');
+      setTimeout(function() {
+        this.$el.remove();
+      }.bind(this), 300);
+    }
+
+  });
+
+})(this.App);
