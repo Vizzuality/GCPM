7eb8033938d93376e4a73e70dc837995f3230c96
Adrián <aloha@adrianperez.info>
2016-09-22 18:42:02 +0200

    print events and projects figures

--- app/assets/javascripts/views/usernav_view.js
+++ app/assets/javascripts/views/usernav_view.js
@@ -13,6 +13,9 @@
       this.user_data = JSON.parse(sessionStorage.getItem('user_data'));
       this.$el.find('.initials').text(this.user_data.user_initial);
       this.$el.find('.c-badge').text(this.user_data.user_project + this.user_data.user_event + 0);
+
+      !!this.user_data.user_project && this.$el.find('.user-stats .prj').html('PROJECTS <span>'+this.user_data.user_project+'</span>');
+      !!this.user_data.user_event && this.$el.find('.user-stats .evn').html('EVENTS <span>'+this.user_data.user_event+'</span>');
     },
 
     toggleMenu: function(e) {
