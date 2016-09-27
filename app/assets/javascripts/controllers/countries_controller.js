f3e66d9a86fa2d7b9d535523421a70d52ae1423d
bitTal <albafjez@gmail.com>
2016-09-27 12:54:56 +0200

    Control user data

--- app/assets/javascripts/controllers/countries_controller.js
+++ app/assets/javascripts/controllers/countries_controller.js
@@ -8,33 +8,17 @@
 
     index: function(params) {
       new App.Presenter.CountriesList(params);
+      new App.Presenter.UserNav(params);
+      user_data = null;
     },
 
     show: function(params) {
-      this.params = params;
-      var layersActived = [2, 6];
-      var layersSpec = this.layersSpec = new App.Collection.LayersSpec();
-      var options = {
-        basemap: null,
-        apiUrl: '/api/map/?group=points&countries[]=' + COUNTRY_ID
-      };
-      // Map view
-      var layersCollection = new App.Collection.Layers();
-      var map = new App.View.Map({
-        el: '#map',
-        collection: this.layersSpec,
-        params: this.params,
-        options: options
-      });
-
-      layersSpec.on('change, reset', function() {
-        map.renderLayers();
-      });
-
-      layersSpec.fetch().done(function() {
-        // This method triggers an event called 'reset'
-        layersSpec.filterByIds(layersActived);
-      });
+      new App.Presenter.Map(params);
+      new App.Presenter.TabNav(params);
+      new App.Presenter.Toolbar(params);
+      new App.Presenter.Breadcrumbs(params);
+      new App.Presenter.UserNav(params);
+      user_data = null;
     }
 
   });
