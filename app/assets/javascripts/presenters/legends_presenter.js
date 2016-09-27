7b466c6552f801e37fe6aa405d5babc9302679ca
Rodrigo Solís <rodrigosolis@Rodrigos-MacBook-Pro.local>
2016-09-27 15:53:53 +0200

    adds map legends and changes svg icon-Oval to icon-marker

---
+++ app/assets/javascripts/presenters/legends_presenter.js
@@ -0,0 +1,75 @@
+(function (App) {
+
+  'use strict';
+
+  var StateModel = Backbone.Model.extend({});
+
+  App.Presenter.Legends = function () {
+    this.initialize.apply(this, arguments);
+  };
+
+  _.extend(App.Presenter.Legends.prototype, {
+
+    initialize: function (params) {
+      this.state = new StateModel();
+      this.legends = new App.View.Legends({ el: '#legends' });
+
+      this.setEvents();
+      this.setSubscriptions();
+
+      this.setState(params);
+    },
+
+    setState: function (params) {
+      this.state
+        .set(_.pick(params, 'data', 'global', 'region', 'country'));
+    },
+
+    getState: function () {
+      return this.state.attributes;
+    },
+
+    setEvents: function () {
+      this.state.on('change', function () {
+        this.renderLegends();
+        App.trigger('Legends:change', this.getState());
+      }, this);
+    },
+
+    setSubscriptions: function () {
+      App.on('Map:change', this.setState, this);
+    },
+
+    renderLegends: function () {
+      var data;
+      var attributes = this.getState();
+
+      if (attributes.data === 'projects') {
+        data = [{ blueCircle: true, description: 'Number of Projects' }];
+        if (typeof attributes.country !== 'undefined') {
+          data = [
+            { blueMarker: true, description: 'Project Leads' },
+            { purpleCircle: true, description: 'Researchers' }
+          ];
+        }
+
+      } else if (attributes.data === 'events') {
+        data = [
+          { orangeCircle: true, description: 'Events Per Region' }
+        ];
+        if (typeof attributes.region !== 'undefined') {
+          data = [
+            { orangeCircle: true, description: 'Number of Events' }
+          ];
+          if (typeof attributes.country !== 'undefined') {
+            data = [
+              { orangeMarker: true, description: 'Events' }
+            ];
+          }
+        }
+      }
+      this.legends.updateData(data);
+    }
+  });
+
+})(this.App);
