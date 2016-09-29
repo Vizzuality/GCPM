909e858db13b3689bc3beb193a58cf4814fc1079
Rodrigo Solís <rodrigosolis@Rodrigos-MacBook-Pro.local>
2016-09-29 19:32:05 +0200

    add styles for events tab in legends bar

--- app/assets/javascripts/presenters/legends_presenter.js
+++ app/assets/javascripts/presenters/legends_presenter.js
@@ -53,7 +53,11 @@
             ];
           break;
         case 'events':
-          data = [{ orangeCircle: true, description: 'Events Per Region' }];
+          data = [
+            { orangeCircle: true, description: 'Events Per Region', number: '1,387' },
+            { orangeSquare: true, description: 'Public Events', number: '1,829' },
+            { whiteSquare: true, description: 'Private Events', number: '16,322' }
+            ];
           break;
         case 'people':
           break;
