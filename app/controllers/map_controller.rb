7e71ef362982c8c0ca2717bf70cf8750f9915497
David Inga <davidsingal@icloud.com>
2016-10-03 22:07:17 +0200

    fixing lists

--- app/controllers/map_controller.rb
+++ app/controllers/map_controller.rb
@@ -6,10 +6,8 @@ class MapController < ApplicationController
     params = request.query_parameters
 
     @page = params.key?(:page) && params[:page] ? params[:page].to_i : 1
-    @title = t 'map'
     @filters = %w(projects events)
     @current_type = params.key?(:data) ? params[:data] : 'projects'
-    @user_data = current_user.present? ? JSON.generate(build_user_data) : nil
 
     limit = 12 + (@page * 9)
 
@@ -33,19 +31,6 @@ class MapController < ApplicationController
   private
 
     def projects_params
-      params.permit(:data, :sortby, :user, :start_date, :end_date, :region, :country, project_types:[], cancer_types:[], organization_types:[], organizations:[], investigators:[])
-    end
-
-    def build_user_data
-      if current_user.name && current_user.email
-        user_initial = current_user.email[0].upcase
-      else
-        user_initial = 'U'
-      end
-      {
-        user_project: Project.where("user_id = #{current_user.id}").count,
-        user_event: Event.where("user_id = #{current_user.id}").count,
-        user_initial: user_initial
-      }
+      params.permit(:data, :sortby, :start_date, :end_date, :region, :country, project_types:[], cancer_types:[], organization_types:[], organizations:[], investigators:[])
     end
 end
