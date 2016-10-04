5ac506d108b759663403208d7d2c8b8a233f958f
David Inga <davidsingal@icloud.com>
2016-10-04 10:33:38 +0200

    lists

--- app/controllers/countries_controller.rb
+++ app/controllers/countries_controller.rb
@@ -39,17 +39,4 @@ class CountriesController < ApplicationController
     def countries_params
       params.permit(:data, :region, :country)
     end
-
-    def build_user_data
-      if current_user.name && current_user.email
-        user_initial = current_user.email[0].upcase
-      else
-        user_initial = 'U'
-      end
-      {
-        user_project: Project.where("user_id = #{current_user.id}").count,
-        user_event: Event.where("user_id = #{current_user.id}").count,
-        user_initial: user_initial
-      }
-    end
 end
