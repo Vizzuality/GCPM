666f27f0d936adb781297db12e185f00de4239f3
bitTal <albafjez@gmail.com>
2016-10-05 16:16:59 +0200

    Projects & events quantity on detail pages

--- app/controllers/investigators_controller.rb
+++ app/controllers/investigators_controller.rb
@@ -15,16 +15,17 @@ class InvestigatorsController < ApplicationController
 
     limit = 12 + (@page * 9)
 
+    @events = Event.fetch_all(investigator: @investigator.id).order('created_at DESC')
+    @projects = Project.fetch_all(investigator: @investigator.id).order('created_at DESC')
+
     if params.key?(:data) && params[:data] == 'events'
-      events = Event.fetch_all(investigator: @investigator.id).order('created_at DESC')
-      @items = events.limit(limit)
-      @more = (events.size > @items.size)
-      @items_total = events.size
+      @items = @events.limit(limit)
+      @more = (@events.size > @items.size)
+      @items_total = @events.size
     else
-      projects = Project.fetch_all(investigator: @investigator.id).order('created_at DESC')
-      @items = projects.limit(limit)
-      @more = (projects.size > @items.size)
-      @items_total = projects.size
+      @items = @projects.limit(limit)
+      @more = (@projects.size > @items.size)
+      @items_total = @projects.size
     end
 
     respond_with(@items)
