900b1346075c8d34da722804e87516482078067a
David Inga <davidsingal@icloud.com>
2016-10-07 12:37:57 +0200

    project detail page

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -2,5 +2,6 @@ class ProjectsController < ApplicationController
   def show
     @project = Project.find(params[:id])
     gon.server_params = { }
+    gon.api_location_path = "/api/map/projects/#{params[:id]}"
   end
 end
