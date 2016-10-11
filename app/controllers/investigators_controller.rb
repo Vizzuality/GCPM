ff56f66a2bd5590c1223ae65989fd5b80619e3c7
Adrián <aloha@adrianperez.info>
2016-10-11 18:19:07 +0200

    investigators people tab

--- app/controllers/investigators_controller.rb
+++ app/controllers/investigators_controller.rb
@@ -8,7 +8,7 @@ class InvestigatorsController < ApplicationController
 
   def show
     @page = params.key?(:page) && params[:page] ? params[:page].to_i : 1
-    @filters = %w(projects events)
+    @filters = %w(projects people events)
     @current_type = params.key?(:data) ? params[:data] : 'projects'
 
     gon.server_params = { 'investigators[]': params[:id] }
@@ -17,11 +17,16 @@ class InvestigatorsController < ApplicationController
 
     @events = Event.fetch_all(investigator: @investigator.id).order('created_at DESC')
     @projects = Project.fetch_all(investigator: @investigator.id).order('created_at DESC')
+    @people = Investigator.fetch_all(investigator: @investigator.id).order('created_at DESC')
 
     if params.key?(:data) && params[:data] == 'events'
       @items = @events.limit(limit)
       @more = (@events.size > @items.size)
       @items_total = @events.size
+    elsif params.key?(:data) && params[:data] == 'people'
+      @items = @people.limit(limit)
+      @more = (@people.size > @items.size)
+      @items_total = @people.size
     else
       @items = @projects.limit(limit)
       @more = (@projects.size > @items.size)
--- app/controllers/organizations_controller.rb
+++ app/controllers/organizations_controller.rb
@@ -17,11 +17,16 @@ class OrganizationsController < ApplicationController
 
     @events = Event.fetch_all(organization: @organization.id).order('created_at DESC')
     @projects = Project.fetch_all(organization: @organization.id).order('created_at DESC')
+    @people = Investigator.fetch_all(organization: @organization.id).order('created_at DESC')
 
     if params.key?(:data) && params[:data] == 'events'
       @items = @events.limit(limit)
       @more = (@events.size > @items.size)
       @items_total = @events.size
+    elsif params.key?(:data) && params[:data] == 'people'
+      @items = @people.limit(limit)
+      @more = (@people.size > @items.size)
+      @items_total = @people.size
     else
       @items = @projects.limit(limit)
       @more = (@projects.size > @items.size)
