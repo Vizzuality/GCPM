56f1b5401ec4e9d31007dbaac34567b1175d5e85
Adrián <aloha@adrianperez.info>
2016-10-11 15:21:10 +0200

    projects as default option

--- app/controllers/cancer_types_controller.rb
+++ app/controllers/cancer_types_controller.rb
@@ -17,19 +17,20 @@ class CancerTypesController < ApplicationController
 
     @events = Event.fetch_all(cancer_types: @cancer_type.id).order('created_at DESC')
     @projects = Project.fetch_all(cancer_types: @cancer_type.id).order('created_at DESC')
-    @people = Investigator.fetch_all({cancer_types: @cancer_type.id}).order('created_at DESC')
+    @people = Investigator.fetch_all(cancer_types: @cancer_type.id).order('created_at DESC')
     if params.key?(:data) && params[:data] == 'events'
       @items = @events.limit(limit)
       @more = (@events.size > @items.size)
       @items_total = @events.size
-    elsif params.key?(:data) && params[:data] == 'projects'
-      @items = @projects.limit(limit)
-      @more = (@projects.size > @items.size)
-      @items_total = @projects.size
     elsif params.key?(:data) && params[:data] == 'people'
       @items = @people.limit(limit)
       @more = (@people.size > @items.size)
       @items_total = @people.size
+    else 
+      # projects by default
+      @items = @projects.limit(limit)
+      @more = (@projects.size > @items.size)
+      @items_total = @projects.size
     end
 
     respond_with(@items)
