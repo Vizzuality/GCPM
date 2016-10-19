dc9625f00019d18c7ad5261346ef1860835ed3f5
bitTal <albafjez@gmail.com>
2016-10-19 14:11:14 +0200

    Redirect to new post when not saved

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,28 +1,37 @@
 class PostsController < InheritedResources::Base
+  before_action :check_user, only: [:new, :create, :index]
+
   def index
-    if !current_user
-      redirect_to new_user_session_path and return
-    else
+    if current_user
       redirect_to user_path(id: current_user.id, data: 'posts')
     end
   end
 
   def new
-    if !current_user
-      redirect_to new_user_session_path and return
-    end
-
     @post = Post.new
   end
 
   def create
+    @post = Post.new(post_params)
     @post.user = current_user
+
+    if @post.save
+      redirect_to post_path(@post.id)
+    else
+      redirect_to new_post_path(error: true)
+    end
   end
 
   private
 
     def post_params
       params.require(:post).permit(:title, :body, :user_id)
+    end
+
+    def check_user
+      if !current_user
+        redirect_to new_user_session_path and return
+      end
     end
 end
 
