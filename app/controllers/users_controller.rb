97fecf2dba48df01ccc1f65393898624568bdf27
Miguel Barrenechea <miguel.barrenechea@vizzuality.com>
2016-10-25 13:05:56 +0200

    Messages: template created

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,7 +11,7 @@ class UsersController < ApplicationController
     end
 
     @page = params.key?(:page) && params[:page] ? params[:page].to_i : 1
-    @filters = %w(network projects posts events)
+    @filters = %w(network projects posts events messages)
     @current_type = params.key?(:data) ? params[:data] : 'projects'
 
     gon.server_params = { 'investigators[]': @investigator.size.positive? ? @investigator.first.id : '0' }
@@ -34,6 +34,10 @@ class UsersController < ApplicationController
       @more = (@posts.size > @items.size)
       @items_total = @posts.size
     elsif params.key?(:data) && params[:data] == 'events'
+      @items = @events.limit(limit)
+      @more = (@events.size > @items.size)
+      @items_total = @events.size
+    elsif params.key?(:data) && params[:data] == 'messages'
       @items = @events.limit(limit)
       @more = (@events.size > @items.size)
       @items_total = @events.size
