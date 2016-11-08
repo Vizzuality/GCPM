7a93bf7e69b06b4eb29c270c520b6cddf335cef6
Miguel Mendoza <info@miguelmendoza.dj>
2016-11-08 11:44:35 +0100

    adds widgets rake task

---
+++ db/data/widgets.rb
@@ -0,0 +1,3 @@
+Widget.create!([
+  {id: 1, name: "test", slug: "test", xname: "eje x", xunit: "cantidad", yname: "eje y", yunit: "tiempo", graphic_type: "line", legend: "sad SD Asd sAD", source: "DDD", query: "sdsd", created_at: "2016-11-07 12:28:04", updated_at: "2016-11-07 12:28:04"}
+])
