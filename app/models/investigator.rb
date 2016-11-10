ad7a6f582e4ad06ef546e2311458201a3ea00b0d
Miguel Mendoza <miguel.mendoza@vizzuality.com>
2016-11-10 13:41:41 +0100

    adds funding scopes

--- app/models/investigator.rb
+++ app/models/investigator.rb
@@ -45,8 +45,10 @@ class Investigator < ApplicationRecord
   scope :inactive,              ->                    { joins(:projects).where('projects.end_date < ?', Time.now).or('projects.start_date > ?', Time.now) }
   scope :by_project_types,      -> project_types      { joins(projects: :project_types).where(project_types: { id: project_types }) }
   scope :by_cancer_types,       -> cancer_types       { joins(projects: :cancer_types).where(cancer_types: { id: cancer_types }) }
+  scope :by_specialities,       -> specialities       { joins(projects: :specialities).where(specialities: { id: specialities }) }
   scope :by_investigators,      -> investigators      { joins(:investigators).where(investigators: { id: investigators }) }
   scope :by_organizations,      -> organizations      { joins(:organizations).where(organizations: { id: organizations }) }
+  scope :by_funding_sources,     -> funding_sources   { joins(projects: :funders).where(funders: { organization_id: funding_sources }) }
   scope :by_organization_types, -> organization_types { joins(organizations: :organization_type).where(organization_types: { id: organization_types }) }
   scope :by_countries,          -> countries          { joins([research_units: [address: :country]]).where(countries: { country_iso_3: countries }) }
   scope :by_regions,            -> regions            { joins(projects: :countries).where(countries: { region_iso: regions }) }
@@ -67,9 +69,11 @@ class Investigator < ApplicationRecord
       investigators = investigators.by_investigators(options[:investigators])           if options[:investigators]
       investigators = investigators.by_project_types(options[:project_types])           if options[:project_types]
       investigators = investigators.by_cancer_types(options[:cancer_types])             if options[:cancer_types]
+      investigators = investigators.by_specialities(options[:specialities])             if options[:specialities]
       investigators = investigators.by_countries(options[:countries])                   if options[:countries]
       investigators = investigators.by_regions(options[:regions])                       if options[:regions]
       investigators = investigators.by_organizations(options[:organizations])           if options[:organizations]
+      investigators = investigators.by_funding_sources(options[:funding_sources])           if options[:funding_sources]
       investigators = investigators.by_organization_types(options[:organization_types]) if options[:organization_types]
       investigators = investigators.by_start_date(options[:start_date])                 if options[:start_date]
       investigators = investigators.by_end_date(options[:end_date])                     if options[:end_date]
