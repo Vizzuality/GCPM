086b93a0697dbc69394d071d004c92bba856cbe9
Miguel Mendoza <info@miguelmendoza.dj>
2016-11-15 13:01:59 +0100

    adds user search

---
+++ app/controllers/api/v1/users_controller.rb
@@ -0,0 +1,14 @@
+module Api
+  module V1
+    class UsersController < ApiController
+      def index
+        users = User.where("email like ? or name like ?", "%#{users_params}%","%#{users_params}%").where.not('confirmed_at is null')
+        render json: users, each_serializer: UserSearchSerializer
+      end
+      private
+      def users_params
+        params.require(:q)
+      end
+    end
+  end
+end
