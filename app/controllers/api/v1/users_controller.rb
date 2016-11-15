f6a307cda90255a70a0110a0373d40a74727e34b
Miguel Mendoza <info@miguelmendoza.dj>
2016-11-15 15:24:03 +0100

    moves user query to model

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -2,7 +2,7 @@ module Api
   module V1
     class UsersController < ApiController
       def index
-        users = User.where("email ilike ? or name ilike ?", "%#{users_params}%","%#{users_params}%").where.not('confirmed_at is null').order('name ASC').limit(10).offset(params[:offset].to_i)
+        users = User.fetch_all(users_params).order('name ASC').limit(10).offset(params[:offset].to_i)
         render json: users, each_serializer: UserSearchSerializer
       end
       private
