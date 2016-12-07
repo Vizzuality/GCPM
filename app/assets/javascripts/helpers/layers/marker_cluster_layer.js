0ada237187739ea0a0e08c2dbef08825a28692d3
Miguel Barrenechea <miguel.barrenechea@vizzuality.com>
2016-12-07 16:57:46 +0100

    Map: remove investigations popup

--- app/assets/javascripts/helpers/layers/marker_cluster_layer.js
+++ app/assets/javascripts/helpers/layers/marker_cluster_layer.js
@@ -54,15 +54,9 @@
     pruneCluster.Cluster.Size = 20;
 
     pruneCluster.BuildLeafletCluster = function(cluster, position) {
-      var data = cluster.lastMarker.data.feature.properties.event ?
-        { events: cluster.population } :
-        { investigations: cluster.population }
-      var htmlContent = infowindowTemplate(data);
-
       var m = new L.Marker(position, {
         icon: pruneCluster.BuildLeafletClusterIcon(cluster)
       });
-      m.bindPopup(htmlContent);
 
       m.on('click', function() {
         // Compute the  cluster bounds (it's slow : O(n))
