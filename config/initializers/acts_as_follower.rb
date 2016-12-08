c306ef1ce84cf95bbc88e8fc699c613f10ee8bd0
Sebastian Schkudlara <sebastian.schkudlara@gmail.com>
2016-12-08 15:49:49 +0100

    Fixes for acts as follower follower_type

--- config/initializers/acts_as_follower.rb
+++ config/initializers/acts_as_follower.rb
@@ -1,2 +1,3 @@
 # By default list of parent classes includes only `[ApplicationRecord, ActiveRecord::Base]`.
-ActsAsFollower.custom_parent_classes = [ ApplicationRecord ]
+# ActsAsFollower.custom_parent_classes = [ApplicationRecord]
+ActsAsFollower.custom_parent_classes = ['Project', 'Organization', 'CancerType', 'Country', 'Event', 'Investigator', 'User']
