3882eaa93ad4d3f7ac0722928b5bbbae067a4cc7
bitTal <albafjez@gmail.com>
2016-12-12 18:23:27 +0100

    Notifications dropdown logic

---
+++ app/assets/javascripts/views/notifications_view.js
@@ -0,0 +1,41 @@
+(function(App) {
+
+  'use strict';
+
+  App.View.Notifications = Backbone.View.extend({
+
+    events: {
+      'click .js-action-notifications': 'handleClick'
+    },
+
+    initialize: function() {
+      this.cache();
+      this.setEvents();
+    },
+
+    cache: function() {
+      this.content = this.$el.find('.dropdown-content');
+    },
+
+    setEvents: function() {
+      $(document).on('click', this.handleDocumentClick.bind(this));
+    },
+
+    handleClick: function() {
+      this.trigger('toggle');
+    },
+
+    toggleDropdown: function(open) {
+      this.content.toggleClass('-open', open);
+    },
+
+    handleDocumentClick: function(e) {
+      if (this.el) {
+        var isContained = this.el.contains(e.target);
+        !isContained && this.trigger('close', this);
+      }
+    },
+
+  });
+
+})(this.App);
