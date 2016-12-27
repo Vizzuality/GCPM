1d57e379e38debd809360d110df4c968b4ba2587
Miguel Mendoza <info@miguelmendoza.dj>
2016-12-27 13:05:35 +0100

    adds AA post

--- app/admin/post.rb
+++ app/admin/post.rb
@@ -8,7 +8,31 @@
 ActiveAdmin.register Post do
   menu parent: "Entities"
   extend Featurable
+  permit_params :title, :body, :graphic_type, :x_axis, :y_axis, :legend, :source, :query
 
+  form do |f|
+    f.semantic_errors
+    f.inputs 'Post Details' do
+      f.input :title
+      f.input :body
+      f.input :user
+    end
+    actions
+  end
+
+  index do
+    selectable_column
+    column :id
+    column :title
+    column :user
+    actions do |obj|
+      if obj.featured?
+        link_to("Unfeature", unfeature_admin_post_path(obj))
+      else
+        link_to("Feature", feature_admin_post_path(obj))
+      end
+    end
+  end
 
 
 
