3ac1e8bcf999b1a8cdc82b4ba987e7b08cce5abc
Sebastian Schkudlara <sebastian.schkudlara@gmail.com>
2017-01-03 10:20:37 +0100

    Force encoding for csv download

--- config/routes.rb
+++ config/routes.rb
@@ -30,17 +30,16 @@ Rails.application.routes.draw do
     patch 'remove_relation',  on: :member
   end
 
-  resources :projects, only: :show
-  resources :events, except: [:index]
+  resources :projects, only:   :show
+  resources :events,   except: :index
   resources :posts
 
   # User profile
   resources :users, only: [:show, :edit, :update], path: :network do
     resources :projects, controller: 'network_projects', except: :index do
-      patch 'remove_relation',  on: :member
+      patch 'remove_relation', on: :member
     end
-
-    resources :events,   controller: 'network_events',   except: :index
+    resources :events, controller: 'network_events', except: :index
   end
 
   resources :notifications, only: :show do
@@ -48,12 +47,11 @@ Rails.application.routes.draw do
   end
 
   # Network
-  get '/network/:id/projects', to: 'users#show'
-
-  post 'follows/:resource/:id', to: 'follows#create', as: :follows
+  get    '/network/:id/projects', to: 'users#show'
+  post   'follows/:resource/:id', to: 'follows#create',  as: :follows
   delete 'follows/:resource/:id', to: 'follows#destroy', as: :follow
 
-  post 'block/:user_id', to: 'follows#block', as: :blocks
+  post   'block/:user_id', to: 'follows#block',   as: :blocks
   delete 'block/:user_id', to: 'follows#unblock', as: :block
 
   resources :searches, path: 'search', controller: 'search', only: :index
@@ -62,13 +60,13 @@ Rails.application.routes.draw do
   #get 'admin/excel-uploader', to: 'admin/excel_uploader#new', as: :admin_excel_uploader
 
   # Mensajes
-  post "/message", to: 'messages#create', as: :message
-  get "/network/:user_id/messages", to: 'messages#index', as: :messages
-  get "/network/:user_id/messages/:id", to: 'messages#show', as: :message_show
-  delete "/messages/:id", to: 'messages#destroy', as: :delete_message
+  post   '/message',                       to: 'messages#create',  as: :message
+  get    '/network/:user_id/messages',     to: 'messages#index',   as: :messages
+  get    '/network/:user_id/messages/:id', to: 'messages#show',    as: :message_show
+  delete '/messages/:id',                  to: 'messages#destroy', as: :delete_message
 
   # Contact form
-  get 'contact', to: 'contact_forms#new', as: :new_contact_form
+  get  'contact',      to: 'contact_forms#new',    as: :new_contact_form
   post 'send-message', to: 'contact_forms#create', as: :send_contact_form
 
   # API
@@ -76,7 +74,7 @@ Rails.application.routes.draw do
     scope module: :v1 do
       resources :regions,            only: [:index, :show]
       resources :cancer_types,       only: [:index, :show], path: '/cancer-types'
-      resources :specialities,       only: [:index], path: '/specialities'
+      resources :specialities,       only: [:index],        path: '/specialities'
       resources :project_types,      only: [:index],        path: 'project-types'
       resources :organization_types, only: [:index],        path: 'organization-types'
       resources :map,                only: [:index]
@@ -85,25 +83,25 @@ Rails.application.routes.draw do
       get '/map/projects/:id', to: 'map#show_project'
       get '/map/download',     to: 'map#csv_download'
 
-      resources :projects,      only: [:index, :show, :update, :create] do
+      resources :projects, only: [:index, :show, :update, :create] do
         resources :memberships, only: [:index, :create, :destroy]
         post '/memberships/:id', to: 'memberships#update'
       end
 
-      resources :investigators,  only: [:index, :show, :update, :create]
-      get '/investigators/:id/graph', to: 'investigators#graph'
-      resources :organizations,  only: [:index, :show]
+      resources :investigators, only: [:index, :show, :update, :create]
+      resources :organizations, only: [:index, :show]
 
-      get 'funding-sources',     to: 'organizations#index'
-      get 'countries',           to: 'countries#index'
-      get 'check_research_unit', to: 'memberships#check_research_unit'
-      get 'lists/countries',     to: 'lists#countries'
-      get 'lists/cancer-types',  to: 'lists#cancer_types'
-      get 'map/projects/:id',    to: 'map#show_project'
-      get 'map/events/:id',      to: 'map#show_event'
-      get 'layer-groups',        to: 'layer_groups#index', as: 'layer_groups'
-      get '/layers',             to: 'layers#index',       as: 'layers'
-      get '/widgets',            to: 'widgets#index',      as: 'widgets'
+      get '/investigators/:id/graph', to: 'investigators#graph'
+      get 'funding-sources',          to: 'organizations#index'
+      get 'countries',                to: 'countries#index'
+      get 'check_research_unit',      to: 'memberships#check_research_unit'
+      get 'lists/countries',          to: 'lists#countries'
+      get 'lists/cancer-types',       to: 'lists#cancer_types'
+      get 'map/projects/:id',         to: 'map#show_project'
+      get 'map/events/:id',           to: 'map#show_event'
+      get 'layer-groups',             to: 'layer_groups#index', as: 'layer_groups'
+      get '/layers',                  to: 'layers#index',       as: 'layers'
+      get '/widgets',                 to: 'widgets#index',      as: 'widgets'
     end
   end
 
