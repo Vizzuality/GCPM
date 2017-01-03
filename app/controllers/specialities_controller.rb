34d7dee7d765ceafc0706998cb84c3e21270c727
Sebastian Schkudlara <sebastian.schkudlara@gmail.com>
2017-01-03 15:11:58 +0100

    Add specialities pages

---
+++ app/controllers/specialities_controller.rb
@@ -0,0 +1,39 @@
+class SpecialitiesController < ApplicationController
+  before_action :set_speciality, only: :show
+  respond_to :html, :js
+
+  def index
+  end
+
+  def show
+    @page = params.key?(:page) && params[:page] ? params[:page].to_i : 1
+    @title = t 'map'
+
+    @filters = %w(projects people)
+    @current_type = params.key?(:data) ? params[:data] : 'projects'
+
+    limit = 12 + (@page * 9)
+
+    @projects = Project.fetch_all(specialities: @speciality.id).order('created_at DESC')
+    @people = Investigator.fetch_all(specialities: @speciality.id).order('created_at DESC')
+
+    if params.key?(:data) && params[:data] == 'people'
+      @items = @people.limit(limit)
+      @more = (@people.size > @items.size)
+      @items_total = @people.size
+    else
+      # projects by default
+      @items = @projects.limit(limit)
+      @more = (@projects.size > @items.size)
+      @items_total = @projects.size
+    end
+
+    respond_with(@items)
+  end
+
+  private
+
+    def set_speciality
+      @speciality = Speciality.find(params[:id])
+    end
+end
