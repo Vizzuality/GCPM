1f18c1ff0f0831a7341f6c707be682fc906d9f3b
Miguel Barrenechea <miguel.barrenechea@vizzuality.com>
2017-01-03 15:46:11 +0100

    Javascript added

--- app/controllers/specialities_controller.rb
+++ app/controllers/specialities_controller.rb
@@ -12,6 +12,11 @@ class SpecialitiesController < ApplicationController
     @filters = %w(projects people)
     @current_type = params.key?(:data) ? params[:data] : 'projects'
 
+    gon.server_params = { 'cancer_types[]': @speciality.id }
+    gon.carto_account = ENV["CARTODB_ACCOUNT"]
+    gon.carto_key = ENV["CARTODB_KEY"]
+    gon.isMobile = browser.device.mobile?
+
     limit = 12 + (@page * 9)
 
     @projects = Project.fetch_all(specialities: @speciality.id).order('created_at DESC')
