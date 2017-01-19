8ef83f4b2b867fd664f1e6157ca5f6984ab57429
Sebastian Schkudlara <sebastian.schkudlara@gmail.com>
2017-01-19 13:27:38 +0100

    Commit out corrupt migration

--- db/migrate/20161104114308_create_specialities.rb
+++ db/migrate/20161104114308_create_specialities.rb
@@ -1,9 +1,9 @@
 class CreateSpecialities < ActiveRecord::Migration[5.0]
   def change
-    # create_table :specialities do |t|
-    #   t.string :name
+    create_table :specialities do |t|
+      t.string :name
 
-    #   t.timestamps
-    # end
+      t.timestamps
+    end
   end
 end
