791fc893fc64b59932b5e1977500fba0c75d6da3
rschumann <sebastian.schkudlara@gmail.com>
2017-03-10 14:20:26 +0100

    Allows admin user to add user to event and show user as creator on events csv download

--- app/admin/event.rb
+++ app/admin/event.rb
@@ -2,7 +2,7 @@ ActiveAdmin.register Event do
   extend Featurable
   menu parent: "Entities", priority: 2
 
-  permit_params :title, :description, :website, :excerpt, :participants, :start_date, :end_date, :private, :online, :address, :address2, :city, :country, :state, :latitude, :longitude, :postcode
+  permit_params :title, :description, :website, :excerpt, :participants, :start_date, :end_date, :private, :online, :address, :address2, :city, :country, :state, :latitude, :longitude, :postcode, :user_id
 
   index do
     selectable_column
@@ -39,7 +39,33 @@ ActiveAdmin.register Event do
       f.input :postcode
       f.input :latitude
       f.input :longitude
+      f.input :user_id, as: :select, collection: User.all.map { |u| ["Name: #{u.display_name}, ID: #{u.id}", u.id] }
     end
     f.actions
+  end
+
+  csv do
+    column(:id)
+    column(:title)
+    column('creator') { |event| event.user.display_name if event.user.present? }
+    column(:description)
+    column(:website)
+    column(:excerpt)
+    column(:participants)
+    column(:start_date)
+    column(:end_date)
+    column(:private)
+    column(:online)
+    column(:address)
+    column(:address2)
+    column(:city)
+    column(:country)
+    column(:state)
+    column(:latitude)
+    column(:longitude)
+    column(:postcode)
+    column(:created_at)
+    column(:updated_at)
+    column(:slug)
   end
 end
