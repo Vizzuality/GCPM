2a74157e9e798e9d25468bd328b4409607689460
Miguel Mendoza <info@miguelmendoza.dj>
2017-06-12 11:01:46 +0200

    fixes RU

--- app/admin/research_unit.rb
+++ app/admin/research_unit.rb
@@ -1,7 +1,7 @@
 ActiveAdmin.register ResearchUnit do
   menu parent: "Entities"
 
-  permit_params :address_id, :project_id
+  permit_params :address_id, :project_id, :investigator_id
   actions :all, except: [:edit]
 
 
@@ -19,7 +19,7 @@ ActiveAdmin.register ResearchUnit do
    form do |f|
     f.semantic_errors
     f.inputs do
-      f.input :investigator_id, as: :select, collection: Investigator.all.map{ |i| [i.name, i.id] }
+      f.input :investigator_id, as: :select, collection: Investigator.all.order(:name).map{ |i| [i.name, i.id] }
       f.input :address_id, as: :select, collection: Address.eager_load(:organization).order('organizations.name').map{ |i| ["#{i.organization.name if i.organization} - #{i.country_name}", i.id] }
     end
     f.actions
